eb022b5a3c22fc15631c04f95a3b17962b348669
Matti Hänninen <matti@mjhanninen.com>
2015-07-20 16:36:15 +0300

    Add directory variables for Emacs
    To automatically enable the editor settings that are in line with
    the guidelines of `CONTRIBUTING.md`.
    
    fixup! Add directory variables for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 4)
+         (fill-column . 80)))
+ ;; Warn about spaces used for indentation:
+ (c-mode . ((eval . (highlight-regexp "^ *"))))
+ (c++-mode . ((eval . (highlight-regexp "^ *"))))
+ (c-mode . ((c-file-style . "ellemtel")))
+ (c++-mode . ((c-file-style . "ellemtel"))))
